2b5e4addcda79bbdadea8aefbf5347084906e99c
Lakki ali <lakki0072022@gmail.com>
2025-04-15 17:22:32 +0530

    changes

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-    # app.run(host='0.0.0.0', debug=True)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', debug=True)
